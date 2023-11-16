3eba41dfc584bffd9d52d5353cf94624b30b05bc
Kazuki Ota <kaota@microsoft.com>
2023-11-16 21:02:44 +0900

    fix

--- src/Plugins/LocationPlugin.cs
+++ src/Plugins/LocationPlugin.cs
@@ -4,18 +4,13 @@ using System.Threading.Tasks;
 
 namespace SKBot.Plugins;
 
-public class LocationInfoPlugin()
+public class LocationInfoPlugin
 {
     [SKFunction, Description("現在いる場所の住所を取得できます。")]
-    public async Task<string> GetCurrentLocationAsync()
-    {
-        return "日本、〒108-0075 東京都港区港南２丁目１６−３ 品川グランドセントラルタワー";
-    }
+    public string GetCurrentLocation() => 
+        "日本、〒108-0075 東京都港区港南２丁目１６−３ 品川グランドセントラルタワー";
 
     [SKFunction, Description("現在いる場所の天気と気温を取得できます。")]
-    public async Task<string> GetWeather()
-    {
-        return "晴れ時々曇り, 降水確率20%, 気温18℃";
-
-    }
+    public string GetWeather() => 
+        "晴れ時々曇り, 降水確率20%, 気温18℃";
 }
